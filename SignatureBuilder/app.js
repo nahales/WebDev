836fa09d754d1d758dd13872d0a1862304521262
Nahal Esmaeili <nesmaeili@myseneca.ca>
2018-11-16 12:36:29 -0500

    Add files via upload

---
+++ SignatureBuilder/app.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const nodemailer = require('nodemailer');
+
+const app = express();
+
+// View engine setup
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Static folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.render('signature');
+});
+
+app.post('/send', (req, res) => {
+  const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>
+      <li>Name: ${req.body.name}</li>
+      <li>Company: ${req.body.company}</li>
+      <li>Email: ${req.body.email}</li>
+      <li>Phone: ${req.body.phone}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `;
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: 'mail.respublica.ca',
+    port: 25,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: '', // generated ethereal user
+        pass: ''  // generated ethereal password
+    },
+    tls:{
+      rejectUnauthorized:false
+    }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+      from: 'RES PUBLICA Signature <donotreply@respublica.ca>;', // sender address
+      to: 'RECEIVEREMAILS', // list of receivers
+      subject: 'Signature Setup - Select full contents of the email and copy', // Subject line
+      text: ${req.body.message}, // plain text body
+      html: ${req.body.message} // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error);
+      }
+      console.log('Message sent: %s', info.messageId);
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+      res.render('signature', {msg:'Email has been sent'});
+  });
+  });
+
+app.listen(8080, () => console.log('Server started...'));
